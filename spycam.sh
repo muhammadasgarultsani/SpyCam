5beb2946dee8efa629e742815f112e29406e136e
Muhammad Asgarul Tsani <muhammadasgarultsani@gmail.com>
2026-05-10 04:34:44 +0800

    add banner ascii

---
+++ spycam.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+banner() {
+  clear
+  printf "\x1b[31;49;1m _______  _______           \x1b[34;1m_______  _______  _______ \n"
+  printf "\x1b[31;49;1m(  ____ \(  ____ )|\     /|\x1b[34;1m(  ____ \(  ___  )(       )\n"
+  printf "\x1b[31;49;1m| (    \/| (    )|( \   / )\x1b[34;1m| (    \/| (   ) || () () |\n"
+  printf "\x1b[31;49;1m| (_____ | (____)| \ (_) / \x1b[34;1m| |      | (___) || || || |\n"
+  printf "\x1b[31;49;1m(_____  )|  _____)  \   /  \x1b[34;1m| |      |  ___  || |(_)| |\n"
+  printf "\x1b[31;49;1m      ) || (         ) (   \x1b[34;1m| |      | (   ) || |   | |\n"
+  printf "\x1b[31;49;1m/\____) || )         | |   \x1b[34;1m| (____/\| )   ( || )   ( |\n"
+  printf "\x1b[31;49;1m\_______)|/          \_/   \x1b[34;1m(_______/|/     \||/     \|\n"
+  printf "\x1b[33;1mSpyCam V1.0\n"
+  printf "\x1b[37;1mCreate by Astral  |  https://github.com/muhammadasgarultsani/SpyCam.git\n"
+}
+banner
